e2faf42d183a77cf8561afa720cb06ae40047627
Hiroto-Saito1 <hiroto.saito.t5@dc.tohoku.ac.jp>
2024-04-09 15:41:10 +0900

    tests modified

--- tests/submit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#PBS -l nodes=1:ppn=8 
-#PBS -q GroupE 
-
-source /opt/intel_2022/setvars.sh --force intel64 
-conda activate mae 
-cd $PBS_O_WORKDIR 
-numactl --interleave=all mpirun -n 8 python /home/hirotosaito/MaterialsProject/mag20230425/magnetic_anisotropy/src/parallel_eigval.py
-rm -f submit.sh.*
